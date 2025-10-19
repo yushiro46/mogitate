29ee3bf09bf3c997bd1022c62e615570c3fb543b
yuu <yushiro.soccer@icloud.com>
2025-10-19 21:10:42 +0900

    更新１

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(ProductsTableSeeder::class);
+        $this->call(SeasonsTableSeeder::class);
+        $this->call(Product_seasonTableSeeder::class);
     }
 }
